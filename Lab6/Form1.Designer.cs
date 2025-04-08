5fbb93c1a79179003f645017fc0aaa57aade0458
AlexanderFurkalo <112818751+AlexanderFurkalo@users.noreply.github.com>
2025-04-09 01:09:41 +0300

    Lab6

---
+++ Lab6/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace Lab6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DirectoriesTreeView = new System.Windows.Forms.TreeView();
+            this.btnLoadDirectory = new System.Windows.Forms.Button();
+            this.btnSearchFile = new System.Windows.Forms.Button();
+            this.textBoxSearchFile = new System.Windows.Forms.TextBox();
+            this.listViewSearchResults = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // DirectoriesTreeView
+            // 
+            this.DirectoriesTreeView.Location = new System.Drawing.Point(26, 52);
+            this.DirectoriesTreeView.Name = "DirectoriesTreeView";
+            this.DirectoriesTreeView.Size = new System.Drawing.Size(258, 261);
+            this.DirectoriesTreeView.TabIndex = 0;
+            // 
+            // btnLoadDirectory
+            // 
+            this.btnLoadDirectory.Location = new System.Drawing.Point(26, 12);
+            this.btnLoadDirectory.Name = "btnLoadDirectory";
+            this.btnLoadDirectory.Size = new System.Drawing.Size(102, 23);
+            this.btnLoadDirectory.TabIndex = 1;
+            this.btnLoadDirectory.Text = "Load Directory";
+            this.btnLoadDirectory.UseVisualStyleBackColor = true;
+            this.btnLoadDirectory.Click += new System.EventHandler(this.btnLoadDirectory_Click);
+            // 
+            // btnSearchFile
+            // 
+            this.btnSearchFile.Location = new System.Drawing.Point(343, 83);
+            this.btnSearchFile.Name = "btnSearchFile";
+            this.btnSearchFile.Size = new System.Drawing.Size(80, 23);
+            this.btnSearchFile.TabIndex = 2;
+            this.btnSearchFile.Text = "Search File";
+            this.btnSearchFile.UseVisualStyleBackColor = true;
+            this.btnSearchFile.Click += new System.EventHandler(this.btnSearchFile_Click);
+            // 
+            // textBoxSearchFile
+            // 
+            this.textBoxSearchFile.Location = new System.Drawing.Point(343, 52);
+            this.textBoxSearchFile.Name = "textBoxSearchFile";
+            this.textBoxSearchFile.Size = new System.Drawing.Size(80, 20);
+            this.textBoxSearchFile.TabIndex = 3;
+            // 
+            // listViewSearchResults
+            // 
+            this.listViewSearchResults.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listViewSearchResults.FullRowSelect = true;
+            this.listViewSearchResults.GridLines = true;
+            this.listViewSearchResults.HideSelection = false;
+            this.listViewSearchResults.Location = new System.Drawing.Point(456, 52);
+            this.listViewSearchResults.Name = "listViewSearchResults";
+            this.listViewSearchResults.Size = new System.Drawing.Size(612, 170);
+            this.listViewSearchResults.TabIndex = 5;
+            this.listViewSearchResults.UseCompatibleStateImageBehavior = false;
+            this.listViewSearchResults.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Name";
+            this.columnHeader1.Width = 167;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Path";
+            this.columnHeader2.Width = 196;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Size";
+            this.columnHeader3.Width = 69;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Created";
+            this.columnHeader4.Width = 70;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1080, 343);
+            this.Controls.Add(this.listViewSearchResults);
+            this.Controls.Add(this.textBoxSearchFile);
+            this.Controls.Add(this.btnSearchFile);
+            this.Controls.Add(this.btnLoadDirectory);
+            this.Controls.Add(this.DirectoriesTreeView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView DirectoriesTreeView;
+        private System.Windows.Forms.Button btnLoadDirectory;
+        private System.Windows.Forms.Button btnSearchFile;
+        private System.Windows.Forms.TextBox textBoxSearchFile;
+        private System.Windows.Forms.ListView listViewSearchResults;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+    }
+}
+
---
+++ Lab6/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Lab6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoadDirectory_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+            if (folderDialog.ShowDialog() == DialogResult.OK)
+            {
+                DirectoriesTreeView.Nodes.Clear();
+                DirectoryInfo rootDir = new DirectoryInfo(folderDialog.SelectedPath);
+                TreeNode rootNode = new TreeNode(rootDir.Name) { Tag = rootDir };
+                DirectoriesTreeView.Nodes.Add(rootNode);
+                LoadDirectories(rootDir, rootNode);
+            }
+        }
+
+        private void LoadDirectories(DirectoryInfo dir, TreeNode node)
+        {
+            try
+            {
+                foreach (DirectoryInfo subDir in dir.GetDirectories())
+                {
+                    TreeNode subNode = new TreeNode(subDir.Name) { Tag = subDir };
+                    node.Nodes.Add(subNode);
+                    LoadDirectories(subDir, subNode);
+                }
+
+                foreach (FileInfo file in dir.GetFiles())
+                {
+                    node.Nodes.Add(new TreeNode(file.Name) { Tag = file });
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                node.Nodes.Add(new TreeNode("Access Denied"));
+            }
+        }
+
+        private void btnSearchFile_Click(object sender, EventArgs e)
+        {
+            listViewSearchResults.Items.Clear();
+
+            if (DirectoriesTreeView.SelectedNode?.Tag is DirectoryInfo startDir && !string.IsNullOrWhiteSpace(textBoxSearchFile.Text))
+            {
+                SearchFiles(startDir, textBoxSearchFile.Text);
+            }
+            else
+            {
+                MessageBox.Show("Please select a directory in the tree and enter a search term.");
+            }
+        }
+
+        private void SearchFiles(DirectoryInfo dir, string pattern)
+        {
+            try
+            {
+                foreach (FileInfo file in dir.GetFiles(pattern))
+                {
+                    ListViewItem item = new ListViewItem(file.Name);
+                    item.SubItems.Add(file.FullName);
+                    item.SubItems.Add(file.Length.ToString());
+                    item.SubItems.Add(file.CreationTime.ToString());
+                    listViewSearchResults.Items.Add(item);
+                }
+
+                foreach (DirectoryInfo subDir in dir.GetDirectories())
+                {
+                    SearchFiles(subDir, pattern); 
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                System.Diagnostics.Debug.WriteLine($"Access denied");
+            }
+        }
+    }
+}
